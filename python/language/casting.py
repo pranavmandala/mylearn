4d118d4f6aceec249477f0ff9058e5603d526678
Pranav Mandala <pranav.mandala07@gmail.com>
2021-12-30 10:53:38 -0800

    Updating Casting Program

--- python/language/casting.py
+++ python/language/casting.py
@@ -29,3 +29,41 @@ print("Converted Value {} -> Type {}".format(z, type(z)))
 z = float(y)
 print("Intial Value {} -> Type {}".format(y, type(y)))
 print("Converted Value {} -> Type {}".format(z, type(z)))
+
+# String to Number
+string = ("cat")
+print("\n")
+print("String to Number")
+newstring = int(string)
+print("Intial Value {} -> Type {}".format(string, type(string)))
+print("Converted Value {} -> Type {}".format(newstring, type(newstring)))
+
+# String to Float
+print("\n")
+print("Stringn to Float")
+newstring = float(string)
+print("Intial Value {} -> Type {}".format(string, type(string)))
+print("Converted Value {} -> Type {}".format(newstring, type(newstring)))
+
+# Number to Boolean
+num = 1
+print("\n")
+print("Number to Boolean")
+newnum = bool(num)
+print("Intial Value {} -> Type {}".format(num, type(num)))
+print("Converted Value {} -> Type {}".format(newnum, type(newnum)))
+
+# Number to Float
+print("\n")
+print("Number to Float")
+newnum = float(num)
+print("Intial Value {} -> Type {}".format(num, type(num)))
+print("Converted Value {} -> Type {}".format(newnum, type(newnum)))
+
+# Float to String
+f = 1
+print("\n")
+print("Float to String")
+newf = str(num)
+print("Intial Value {} -> Type {}".format(f, type(f)))
+print("Converted Value {} -> Type {}".format(newf, type(newf)))
